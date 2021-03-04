ade1255257ed641aa051f58ad7ba3aaecc3d0263
mounirchebbi <2157@holbertonschool.com>
2021-03-04 16:37:01 +0100

    quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -6,7 +6,7 @@ void lomuto_sort(int *array, size_t size, int left, int right);
 void quick_sort(int *array, size_t size);
 
 /**
- * swap_ints - Swap two integers in an array.
+ * swaper - Swap two integers in an array.
  * @n1: first int
  * @n2: second int
  */
